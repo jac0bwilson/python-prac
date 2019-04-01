f4f220a3af1831c90653999db2250c4aa80ffe43
Jacob Wilson <43655948+jac0bwilson@users.noreply.github.com>
2019-04-01 15:39:34 +0100

    Challenge 3

---
+++ challenge3.py
@@ -0,0 +1,7 @@
+import urllib.request
+import re
+
+html = urllib.request.urlopen("http://www.pythonchallenge.com/pc/def/equality.html").read().decode()
+data = re.findall("<!--(.*?)-->", html, re.DOTALL)[-1]
+
+print("".join(re.findall("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+", data)))
