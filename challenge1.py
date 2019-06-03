b0ba5a32a9562a73f7b7b8451d81c16dd27f1e7f
Jacob Wilson <jacobswilsonglos@yahoo.co.uk>
2019-06-03 22:34:10 +0100

    Removed old files

--- challenge1.py
+++
@@ -1,3 +0,0 @@
-url = "http://www.pythonchallenge.com/pc/def/map.html"
-translation = url.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
-print (url.translate(translation))
--- challenge2.py
+++
@@ -1,9 +0,0 @@
-f = open("challenge2-data.txt", "r")
-output = ""
-lines = f.readlines()
-for line in lines:
-    for char in line:
-        if char.isalpha():
-            output += char
-
-print (output)
